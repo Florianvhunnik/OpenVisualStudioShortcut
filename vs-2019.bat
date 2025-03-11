92e6ab85b2d3d98b67ebbff1ca43fe5768c36f7d
Florian <florian2006email@gmail.com>
2025-03-11 17:25:37 +0100

    Add bat files for different versions

---
+++ vs-2019.bat
@@ -0,0 +1,6 @@
+@echo off
+for %%i in ("%cd%\*.sln") do (
+    "C:\Program Files\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" "%%i"
+    exit
+)
+"C:\Program Files\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" %1
