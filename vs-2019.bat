f9679be9880d4f94e3e501650d805c54ae8fba4c
Florian <florian2006email@gmail.com>
2025-03-12 14:25:54 +0100

    (feature) Check first if the path exists and some imporvements

--- vs-2019.bat
+++ vs-2019.bat
@@ -1,6 +1,15 @@
 @echo off
+set "VS_PATH=C:\Program Files\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe"
+
+REM Controleer of Visual Studio bestaat
+if not exist "%VS_PATH%" (
+    echo Error: Visual Studio executable not found! Please check the path.
+    exit /b 1
+)
+
 for %%i in ("%cd%\*.sln") do (
-    "C:\Program Files\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" "%%i"
+    start "" "%VS_PATH%" "%%i"
     exit
 )
-"C:\Program Files\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" %1
+
+start "" "%VS_PATH%" %1
--- vs-2022.bat
+++ vs-2022.bat
@@ -1,6 +1,14 @@
 @echo off
+set "VS_PATH=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
+
+if not exist "%VS_PATH%" (
+    echo Error: Visual Studio executable not found! Please check the path.
+    exit /b 1
+)
+
 for %%i in ("%cd%\*.sln") do (
-    "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe" "%%i"
+    start "" "%VS_PATH%" "%%i"
     exit
 )
-"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe" %1
+
+start "" "%VS_PATH%" %1
